dc31ab264bc950d4f1eb375452a8f5a52e93a91c
hansw <17835279855@163.com>
2022-01-13 14:45:39 +0800

    2022.01.13 114.Flatten Binary Tree to Linked List

--- leetcode/0114.Flatten-Binary-Tree-to-Linked-List/114.Flatten Binary Tree to Linked List.go
+++ leetcode/0114.Flatten-Binary-Tree-to-Linked-List/114.Flatten Binary Tree to Linked List.go
@@ -67,28 +67,23 @@ func flatten2(root *TreeNode) {
 
 // 原地工作
 func flatten3(root *TreeNode) {
-	if root == nil {
-		return
-	}
 	// 先序，则左子树在右子树前
-	// 我们用root表示下一个待处理的节点，循环进行以下几步
+	// 我们用root存放下一个待处理的节点，循环进行以下几步
 	// 如果root为空，则退出循环，否则：
 	// 		如果其有左子树，则将左子树放到root.Right（下一个处理的节点），并使左子树的最右节点指向原来的root.Right，然后将root.Left置空，将root后移
 	// 		如果其没有左子树，则只将root后移
 	for root != nil {
 		if root.Left != nil {
-			tmp := root.Right
-			// 将左子树放到root.Right
-			root.Right = root.Left
+			next := root.Left
 
-			// tmp放到root左子树的最右节点后
+			// root.Right放到root左子树的最下最右节点后
 			newTail := root.Left
 			for newTail.Right != nil {
 				newTail = newTail.Right
 			}
-			newTail.Right = tmp
+			newTail.Right = root.Right
 
-			root.Left = nil
+			root.Left, root.Right = nil, next
 		}
 		// 后移
 		root = root.Right
