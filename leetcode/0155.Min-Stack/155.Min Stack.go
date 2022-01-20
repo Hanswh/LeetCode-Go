120375303ff1c5c368dcfe5cb8252fdfcde07b6b
hansw <17835279855@163.com>
2022-01-20 20:05:53 +0800

    2022.01.20 83.Remove Duplicates from Sorted List 155.Min Stack

---
+++ leetcode/0155.Min-Stack/155.Min Stack.go
@@ -0,0 +1,44 @@
+package leetcode
+
+import "math"
+
+// MinStack 辅助栈
+// 每次插入和删除时同步操作这个辅助栈，辅助栈中保存最小元素
+type MinStack struct {
+	stack    []int
+	minStack []int
+}
+
+func Constructor() MinStack {
+	return MinStack{
+		stack:    []int{},
+		minStack: []int{math.MaxInt64},
+	}
+}
+
+// Push 插入元素时，向辅助栈中插入新元素和辅助栈栈顶中较小的
+func (this *MinStack) Push(x int) {
+	this.stack = append(this.stack, x)
+	top := this.minStack[len(this.minStack)-1]
+	this.minStack = append(this.minStack, min(x, top))
+}
+
+func (this *MinStack) Pop() {
+	this.stack = this.stack[:len(this.stack)-1]
+	this.minStack = this.minStack[:len(this.minStack)-1]
+}
+
+func (this *MinStack) Top() int {
+	return this.stack[len(this.stack)-1]
+}
+
+func (this *MinStack) GetMin() int {
+	return this.minStack[len(this.minStack)-1]
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
